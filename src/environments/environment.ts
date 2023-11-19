364a6c04395d78ea1481d2379af31c8377e365c0
bitxuji <bitxuji@gmail.com>
2023-11-19 13:33:20 +0900

    ng generate environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyCQz6Kr82cnmVmxt922f6dnktT3LUTLnik",
+        authDomain: "fir-demo-c01e7.firebaseapp.com",
+        databaseURL: "https://fir-demo-c01e7-default-rtdb.firebaseio.com",
+        projectId: "fir-demo-c01e7",
+        storageBucket: "fir-demo-c01e7.appspot.com",
+        messagingSenderId: "672204735625",
+        appId: "1:672204735625:web:6d8541806409161e3df258",
+        measurementId: "G-SNG01C8CC2"
+    }
+};
