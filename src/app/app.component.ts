c94d7b8a3e6b085fe9a12f5a9d2e022afd2c3f0c
bitxuji <bitxuji@gmail.com>
2023-11-19 17:41:15 +0900

    Adding an Object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,6 @@
-import { AngularFireDatabase } from '@angular/fire/compat/database'
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database'
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
 
-
-export interface author {
-  name: string,
-  students: number,
-  isPremium: boolean
-}
 
 @Component({
   selector: 'app-root',
@@ -15,12 +8,25 @@ export interface author {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses$;
-  course$;
-  author$: Observable<author>;
+  courses$: AngularFireList<any>;
+  courses;
+
   constructor(db: AngularFireDatabase) {
-    this.courses$ = db.list('/courses').valueChanges();
-    this.course$ = db.object('/courses/1').valueChanges();
-    this.author$ = (db.object('/authors/profile1').valueChanges()) as Observable<author>;
+    this.courses$ = db.list('/courses');
+    this.courses = db.list('/courses').valueChanges();
+  }
+  add(course: HTMLInputElement) {
+    this.courses$.push({
+      name: course.value,
+      price: 150,
+      isLive: true,
+      sections: [
+        { title: 'Component' },
+        { title: 'Directive' },
+        { title: 'Template' }
+      ]
+    }
+    );
+    course.value = '';
   }
 }
